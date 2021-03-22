aaf5c8217d8417917de50b73313c5b10cbb0da21
Sofia Lopez <sofiabelenlopez00@gmail.com>
2021-03-22 15:30:57 +0300

    Working on radial distribution function

--- rdf.py
+++ rdf.py
@@ -5,56 +5,69 @@ import seaborn as sns
 def getRDF(data, tsteps, rsteps, npart):
     g = np.zeros(rsteps)
     dr = rmax / rsteps
-    rho = npart / rmax**2
     
+    totalpart = npart
     for t in range(tsteps):
         for i in range(npart):
-            for j in range(i + 1, npart):
-                xij2 = (data['rx'][t * npart + i] - data['rx'][t * npart + j])**2
-                yij2 = (data['ry'][t * npart + i] - data['ry'][t * npart + j])**2
-                zij2 = (data['rz'][t * npart + i] - data['rz'][t * npart + j])**2
+            for j in range(npart):
+                xij2 = (data['rx'][t * npart + i]\
+                        - data['rx'][t * npart + j])**2
+                yij2 = (data['ry'][t * npart + i]\
+                        - data['ry'][t * npart + j])**2
+                zij2 = (data['rz'][t * npart + i]\
+                        - data['rz'][t * npart + j])**2
                 rij2 = xij2 + yij2 + zij2
     
-                if(rij2 < rmax**2):
-                    g[int(np.sqrt(rij2) / dr)] += 2
+                if(rij2 < rmax**2 and i != j):
+                    ## We found a particle in spherical shell 
+                    ## rij + dr
+                    g[int(np.sqrt(rij2) / dr)] += 1
+                    totalpart += 1
     
+    ## Average density in a sphere or radius rmax
+    V = 4 / 3 * np.pi * rmax**3
+    rho = totalpart / npart / V
+
     for i in range(rsteps):
         rInner = rmax / rsteps * i
         rOuter = rmax / rsteps * (i + 1)
-        V = 4 / 3 * np.pi * (rOuter**3 - rInner**3)
+        # V = 4 / 3 * np.pi * (rOuter**3 - rInner**3)
+        V = 4 * np.pi * rInner**2 * dr
         g[i] /= V
     
     g /= tsteps * rho
     return g
 
-data1 = np.genfromtxt('data1', names=True)
-data2 = np.genfromtxt('data2', names=True)
-data3 = np.genfromtxt('data3', names=True)
+data = np.genfromtxt('dataYarnell', names=True)
+# data2 = np.genfromtxt('data2', names=True)
+# data3 = np.genfromtxt('data3', names=True)
 
+sigma = 3.405 ## Angstrom
 tsteps = 100
-rsteps = 200
-rmax = 5.03968338
-npart = 64
+rsteps = 500
+rmax = 8 / sigma
+npart = 343
 dr = rmax / rsteps
 
-g1 = getRDF(data1, tsteps, rsteps, npart)
-g2 = getRDF(data2, tsteps, rsteps, npart)
-g3 = getRDF(data3, tsteps, rsteps, npart)
+g = getRDF(data, tsteps, rsteps, npart)
+# g2 = getRDF(data2, tsteps, rsteps, npart)
+# g3 = getRDF(data3, tsteps, rsteps, npart)
 
 sns.set(context='notebook', style='darkgrid')
 fig, ax = plt.subplots(figsize=(6, 6))
 
-xvalues = np.arange(0, rmax, dr)
-ax.plot(xvalues, g3, label=r'Gas and Solid: $T = 0.3$')
-ax.plot(xvalues, g2, label=r'Gas and Liquid: $T = 1.0$')
-ax.plot(xvalues, g1, label=r'Fluid: $T = 3.0$')
+xvalues = np.arange(0, rmax, dr) * sigma
+ax.plot(xvalues, g, label=r'$T = 1.409, \rho = 0.84$ (Fluid)')
+# ax.plot(xvalues, g2, label=r'Gas and Liquid: $T = 1.0$')
+# ax.plot(xvalues, g1, label=r'Fluid: $T = 3.0$')
 
-ax.set_xlabel(r'$r$')
+ax.set_xlim(left=3)
+ax.set_ylim(bottom=0)
+ax.set_xlabel(r'$r (\AA)$')
 ax.set_ylabel(r'$g(r)$')
-
 ax.legend()
 
-fig.savefig("media/rdf.pdf")
-fig.savefig("media/rdf.svg")
-fig.savefig("media/rdf.png", dpi=200)
+fig.savefig("media/rdfYarnell.pdf")
+fig.savefig("media/rdfYarnell.svg")
+fig.savefig("media/rdfYarnell.png", dpi=200)
 plt.show()
--- run.sh
+++ run.sh
@@ -14,37 +14,34 @@
 # python tm.py
 
 ### Radial distribution function
+N=343
+rho=0.840874054 ## 0.0213 * (3.405)^3
+T=1.409411764 ## 85 / 119.8
+echo "rx ry rz vx vy vz" > dataYarnell
+
+START=$(date +%s.%N)
+
+for (( i=0; i<10; i++ ))
+do
+    ./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.001 -ns 10000 -fs 100 >> dataYarnell
+    echo "i-th iteration done!"
+done
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "Total time: $DIFF"
+
+python rdf.py
+
+### Diffusion
 # N=343
-# rho=0.5
-# echo "rx ry rz vx vy vz" > data1
-# echo "rx ry rz vx vy vz" > data2
-# echo "rx ry rz vx vy vz" > data3
+# rho=0.8
+# T=2 ## Fluid
+# echo "rx ry rz vx vy vz" > dataDif
 # 
-# T=3 ## Fluid
-# ./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.001 -ns 100000 -fs 1000 >> data1
-# 
-# echo "First done!"
-# 
-# T=1.0 ## Gas and Liquid
-# ./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.001 -ns 100000 -fs 1000 >> data2
-# 
-# echo "Second done!"
-# 
-# T=0.3 ## Gas and solid
-# ./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.001 -ns 100000 -fs 1000 >> data3
+# ./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.0001 -ns 100000 -fs 1000 >> dataDif
 # 
 # echo "Almost done!"
 # 
-# python rdf.py
-
-### Diffusion
-N=343
-rho=0.8
-T=2 ## Fluid
-echo "rx ry rz vx vy vz" > dataDif
-
-./mdlj -uf -N $N -rho $rho -T0 $T -T $T -dt 0.0001 -ns 100000 -fs 1000 >> dataDif
-
-echo "Almost done!"
-
-python diffusion.py
+# python diffusion.py
